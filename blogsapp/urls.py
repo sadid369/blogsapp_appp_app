617080b40cbcbc3b8dcb41a1f17dc233971dc908
sadid369 <sadid.jones@gmail.com>
2026-01-03 09:19:28 +0600

    Add blog detail view and update home template with blog links

--- blogsapp/urls.py
+++ blogsapp/urls.py
@@ -5,8 +5,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
+from blogs import views as blog_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('category/',include('blogs.urls'))
+    path('category/',include('blogs.urls')),
+    path('<slug:slug>/', blog_views.blogs, name='blogs'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
